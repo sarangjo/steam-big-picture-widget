27d7b34c6e2cff1f81860e795cdfa8fcb764ad52
Sarang Joshi <sarangj@msn.com>
2020-11-12 01:28:02 -0800

    Initial commit

---
+++ SteamBigPicture/BigPicture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace BigPictureWidget
+{
+    class BigPicture
+    {
+        static string uriToLaunch = @"steam://open/bigpicture";
+
+        public static IAsyncOperation<bool> Open()
+        {
+            Uri uri = new Uri(uriToLaunch);
+            return Windows.System.Launcher.LaunchUriAsync(uri);
+        }
+    }
+}
